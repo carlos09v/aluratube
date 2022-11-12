ffa4ae3ba7aef17e2b4ab8316d3e6d9be9dea8fe
Carlos Vinicius <carlos09v@gmail.com>
2022-11-11 23:01:35 -0300

    copy fails

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1>Aluratube</h1>
 <h2>Veja o projeto no <a href="https://www.figma.com/file/1acrju7CLwHkSh6e7xEk9h/Aluratube?node-id=5%3A2">Figma 🔖</a></h2>
 <div align='center'>
- <img height='500' src="https://github.com/carlos09v/aluratube/blob/main/aluratube/src/assets/demo_dark_01.png?raw=true" alt="Preview">
+ <img height='500' width='700' src="https://github.com/carlos09v/aluratube/blob/main/aluratube/src/assets/demo_dark_01.png?raw=true" alt="Preview">
 </div>
 <h3>Instrutores: <a href="https://www.instagram.com/paulo_hipster/" target='_blank'>Paulo Silveira</a> | <a href="https://www.instagram.com/nayanne.tech/" target='_blank'>Nayanne Lopes</a> | <a href="https://github.com/omariosouto" target='_blank'>Mario Souto (DevSoutinho)</a></h3>
 <p>Projeto realizado no período de 07 a 11 de Novembro - 2022</p>
--- aluratube/pages/index.tsx
+++ aluratube/pages/index.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import config from '../src/api/config.json'
 
 import Header from "../src/components/Header";
 import TimeLine from '../src/components/TimeLine';
 import Menu from '../src/components/Menu';
+import { videoService } from '../src/services/videoService';
+
 
 export default function Home() {
+  const service = videoService()
   const [valorDoFiltro, setValorDoFiltro] = useState('')
+  const [playlists, setPlaylists] = useState({})
+
+  // Efeito Colateral
+  /* - Supabase - (Desativado no MOMENTO !!)
+  useEffect(() => {
+    service.getAllVideos()
+      .then(dados => {
+        const novasPlaylists = { ...playlists }
+        dados.data.forEach(video => {
+            if (!novasPlaylists[video.playlist]) novasPlaylists[video.playlist] = []
+          
+            novasPlaylists[video.playlist].push(video)
+        })
+        setPlaylists(novasPlaylists)
+  })
+  }, [])
+  */
 
   return (
     <div style={{
